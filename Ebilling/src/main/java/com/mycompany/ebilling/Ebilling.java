736dd540a67051b11cfa9b4ba8d7b6de2db991f3
konamonimahesh <93281089+konamonimahesh@users.noreply.github.com>
2023-04-01 20:34:36 +0530

    Add files via upload

---
+++ Ebilling/src/main/java/com/mycompany/ebilling/Ebilling.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ebilling;
+
+/**
+ *
+ * @author konam
+ */
+public class Ebilling {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
